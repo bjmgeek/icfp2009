2d3e3c2c936f8c4834c626e828b0aa937c840499
bminton <bminton@b32f7ace-148a-414d-94d2-8d341d115642>
2009-06-30 00:41:17 +0000

    initial placeholder for virtual machine
    git-svn-id: file:///home/bminton/.svn_repository/icfp-2009@27 b32f7ace-148a-414d-94d2-8d341d115642

---
+++ ovm.c
@@ -0,0 +1,13 @@
+/* Orbital Virtual Machine
+ * ICFP 2009 contest entry
+ * Brian Minton <brian@minton.name>
+ * $Id$
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main( int argc, char **argv) 
+{
+	return EXIT_SUCCESS;
+}
